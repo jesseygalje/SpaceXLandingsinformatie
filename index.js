8c8c960e2fa14ecf1166f842a8505d106f1caa13
jesseygalje <jesseygalje@users.noreply.github.com>
2021-04-07 12:07:22 +0200

    3e api toevoegen

--- index.js
+++ index.js
@@ -21,30 +21,29 @@ map.on('load', function () {
 	// makes a selection
 	geocoder.on('result', function (ev) {
 	  console.log(ev.result.center);
-    document.getElementById('coordinaten').innerHTML = ev.result.center[0]+ '-' + ev.result.center[1];
     getAPIdata(ev.result.center[0], ev.result.center[1]);
 	});
 });
 
-function getAPIdata(ingevoerdeLon, ingeveordeLat) {
+function getAPIdata(coordinaat1, coordinaat2) {
 
 	// construct request
-	var request = 'https://api.openweathermap.org/data/2.5/weather?appid=ebad1f3488ab22a8a5ecbb867c179d3b&lon=' + ingevoerdeLon + '&lat=' + ingeveordeLat;
+	var verzoek = 'https://api.openweathermap.org/data/2.5/weather?appid=ebad1f3488ab22a8a5ecbb867c179d3b&lon=' + coordinaat1 + '&lat=' + coordinaat2;
 
 	// get current weather
-	fetch(request)	
+	fetch(verzoek)	
 	
 	// parse response to JSON format
-	.then(function(response) {
-		return response.json();
+	.then(function(uitkomst) {
+		return uitkomst.json();
 	})
 	
 	// do something with response
-	.then(function(response) {
+	.then(function(uitkomst) {
 		// show full JSON object
 		// console.log(response);
 		var weatherBox = document.getElementById('weather');
-		weatherBox.innerHTML = response.main.temp - 273.15;
+		weatherBox.innerHTML = uitkomst.main.temp - 273.15;
 		//weatherBox.innerHTML = response.weather[0].description;
 		//weatherBox.innerHTML = response.main.temp;
 
@@ -55,37 +54,52 @@ function getAPIdata(ingevoerdeLon, ingeveordeLat) {
 }
 
 // init data stream
-getAPIdata();
+getAPIdata(locatie1, locatie2);
 
-// datum en tijd
-function lopendeTijd() {
-	var moment = new Date();
-	
-	function addLeadingZero(number) {
-		if(number < 10){
-			return '0' + number;
-		}
-		return number
-	}
-	
-	var seconds = addLeadingZero(moment.getSeconds());
-	var minutes = addLeadingZero(moment.getMinutes());
-	var hours = addLeadingZero(moment.getHours());
-	
-	// if (ingevoerdeLon > 3 && ingevoerdeLon < 6) {
-	// 	hours+1
-	// }
+//zonsopkomst zonsondergang api
+function getAPIdata(locatie1, locatie2) {
+
+	var vraag = 'https://api.ipgeolocation.io/timezone?apiKey=09f72fdd0b864fa19e9f653e337489dc&lat=' + locatie2 + '&long=' + locatie1;
+
+	fetch(vraag)
+
+	.then(function(resultaat) {
+		return resultaat.json();
+	})
+
+	.then(function(resultaat) {
+		// console.log(resultaat);
+		var tijdzone = document.getElementById('kloktijd');
+		tijdzone.innerHTML = resultaat.dat_time_txt;
+	});
+
+}
 
 
-	document.getElementById('kloktijd').innerHTML = hours + ':' + minutes + ':' + seconds;
-	document.getElementById('kloktijd').style.textAlign = "center";
-	}
+// // datum en tijd met timezone
+// function lopendeTijd() {
+// 	var moment = new Date();
 	
-	var autoklok = setInterval(lopendeTijd, 1000);
+// 	function addLeadingZero(number) {
+// 		if(number < 10){
+// 			return '0' + number;
+// 		}
+// 		return number
+// 	}
 	
-	lopendeTijd();
-	
-	dagMoment = new Date();
+// 	var seconds = addLeadingZero(moment.getSeconds());
+// 	var minutes = addLeadingZero(moment.getMinutes());
+// 	var hours = addLeadingZero(moment.getHours());
 
-	var huidigeMaand = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-	document.getElementById('klokdatum').innerHTML = dagMoment.getDate() + ' ' + huidigeMaand[dagMoment.getMonth()] + ' ' + dagMoment.getFullYear();
+// 	document.getElementById('kloktijd').innerHTML = hours + ':' + minutes + ':' + seconds;
+// 	document.getElementById('kloktijd').style.textAlign = "center";
+// 	}
+	
+// 	var autoklok = setInterval(lopendeTijd, 1000);
+	
+// 	lopendeTijd();
+	
+// 	dagMoment = new Date();
+
+// 	var huidigeMaand = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+// 	document.getElementById('klokdatum').innerHTML = dagMoment.getDate() + ' ' + huidigeMaand[dagMoment.getMonth()] + ' ' + dagMoment.getFullYear();
