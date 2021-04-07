a9ba9d6dffed3fa4a466350254e9f5a6c58d3bd9
jesseygalje <jesseygalje@users.noreply.github.com>
2021-04-07 20:45:40 +0200

    eerste breakpoint

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-// Set api token
+//map
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVzc2V5Z2FsamUiLCJhIjoiY2ttbHRwYXZhMGd0ZjJxcnoxcDBhdTN2OCJ9.CHz3IaXGNewSEGhdPgm7qA';
 
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v11',
-	center: [-79.4512, 43.6568],
-	zoom: 13
+	center: [4.363406, 52.031223],
+	zoom: 8
 });
 
-// Add the control to the map.
 var geocoder = new MapboxGeocoder({
 	accessToken: mapboxgl.accessToken,
 	mapboxgl: mapboxgl
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
 map.on('load', function () {
-	// Listen for the `geocoder.input` event that is triggered when a user
-	// makes a selection
 	geocoder.on('result', function (ev) {
-	  console.log(ev.result.center);
     getAPIdata(ev.result.center[0], ev.result.center[1]);
 	});
 });
 
-function getAPIdata(coordinaat1, coordinaat2) {
 
-	// construct request
-	var verzoek = 'https://api.openweathermap.org/data/2.5/weather?appid=ebad1f3488ab22a8a5ecbb867c179d3b&lon=' + coordinaat1 + '&lat=' + coordinaat2;
+
+//zonsopkomst en zonsondergang
+function getAPIdata(locatie1, locatie2) {
+	var vraag = 'https://api.sunrise-sunset.org/json?lat=' + locatie2 + '&lng=' + locatie1 + '&date=today';
+
+	fetch(vraag)
+
+	.then(function(antwoord) {
+		return antwoord.json();
+	})
+
+	.then(function(antwoord) {
+		// console.log(antwoord);
+		document.getElementById('zonsopkomst').innerHTML = 'De zon komt op om: ' + antwoord.results.sunrise + ' UTC';
+		document.getElementById('zonsondergang').innerHTML = 'De zon gaat onder om: ' + antwoord.results.sunset + ' UTC';
+	})
+
+	//
+	var verzoek = 'https://api.openweathermap.org/data/2.5/weather?appid=ebad1f3488ab22a8a5ecbb867c179d3b&lon=' + locatie1 + '&lat=' + locatie2;
 
 	// get current weather
 	fetch(verzoek)	
@@ -41,80 +55,15 @@ function getAPIdata(coordinaat1, coordinaat2) {
 	// do something with response
 	.then(function(uitkomst) {
 		// show full JSON object
-		// console.log(response);
-		// var weatherBox = document.getElementById('weather');
-		document.getElementById('weather').innerHTML = Math.floor(uitkomst.main.temp - 273.15) + '&#176;C <br>';
-		//weatherBox.innerHTML = response.weather[0].description;
-		//weatherBox.innerHTML = response.main.temp;
-
-		// var degC = Math.floor(response.main.temp - 273.15);
-		// var weatherBox = document.getElementById('weather');
-		// weatherBox.innerHTML = degC + '&#176;C <br>';
+		// console.log(uitkomst);
+		document.getElementById('weather').innerHTML = Math.floor(uitkomst.main.temp - 273.15) + '&#176;C <br>' + uitkomst.weather[0].description;
 	});
+
 }
 
-// init data stream
 getAPIdata();
 
-//zonsopkomst zonsondergang api
-// function getAPIdata(locatie1, locatie2) {
+dagMoment = new Date();
 
-// 	var vraag = 'https://timezone-by-location.p.rapidapi.com/timezone?lon=' + locatie1 + '&lat=' + locatie2 + '&s=0&c=1';
-
-// 	fetch(vraag)
-
-// 	.then(function(resultaat) {
-// 		return resultaat.json();
-// 	})
-
-// 	.then(function(resultaat) {
-// 		// console.log(resultaat);
-// 		var tijdzone = document.getElementById('kloktijd');
-// 		tijdzone.innerHTML = resultaat.date_time_txt;
-// 	});
-
-// 	fetch("https://timezone-by-location.p.rapidapi.com/timezone?lon=" + locatie1 + "&lat=" + locatie2 + "&s=0&c=1", {
-// 		"method": "GET",
-// 		"headers": {
-// 			"x-rapidapi-key": "9cd70da1admsh6d815dd932f1372p1e077bjsnbaeb25c34ea1",
-// 			"x-rapidapi-host": "timezone-by-location.p.rapidapi.com"
-// 		}
-// 	})
-// 	.then(response => {
-// 		console.log(response);
-// 	})
-// 	.catch(err => {
-// 		console.error(err);
-// 	});
-// }
-
-// getAPIdata();
-
-
-// // datum en tijd met timezone
-// function lopendeTijd() {
-// 	var moment = new Date();
-	
-// 	function addLeadingZero(number) {
-// 		if(number < 10){
-// 			return '0' + number;
-// 		}
-// 		return number
-// 	}
-	
-// 	var seconds = addLeadingZero(moment.getSeconds());
-// 	var minutes = addLeadingZero(moment.getMinutes());
-// 	var hours = addLeadingZero(moment.getHours());
-
-// 	document.getElementById('kloktijd').innerHTML = hours + ':' + minutes + ':' + seconds;
-// 	document.getElementById('kloktijd').style.textAlign = "center";
-// 	}
-	
-// 	var autoklok = setInterval(lopendeTijd, 1000);
-	
-// 	lopendeTijd();
-	
-// 	dagMoment = new Date();
-
-// 	var huidigeMaand = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-// 	document.getElementById('klokdatum').innerHTML = dagMoment.getDate() + ' ' + huidigeMaand[dagMoment.getMonth()] + ' ' + dagMoment.getFullYear();
+var huidigeMaand = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+document.getElementById('datum').innerHTML = dagMoment.getDate() + ' ' + huidigeMaand[dagMoment.getMonth()] + ' ' + dagMoment.getFullYear();
